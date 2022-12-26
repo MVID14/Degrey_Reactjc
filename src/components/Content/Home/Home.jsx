c2a6340689457d939ce14e34c7cc1a0d63c6dac3
MVID14 <minhdz142001@gmail.com>
2022-12-26 20:18:55 +0700

    update

--- src/components/Content/Home/Home.jsx
+++ src/components/Content/Home/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 
 import './home.scss';
-<<<<<<< HEAD
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 // import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
@@ -28,14 +27,6 @@ const SLIDE2 = [
         url: 'https://file.hstatic.net/1000281824/file/bannernoel_6a4f8c68d85a47a893bcfdb100dffffd.jpg',
     },
 ];
-=======
-// import images from '../../../assets/img/slide.webp';
-import images1 from '../../../assets/img/slide1.webp';
-import images2 from '../../../assets/img/slide2.webp';
-
-import { GrFormPrevious } from 'react-icons/gr';
-import { GrFormNext } from 'react-icons/gr';
->>>>>>> ba136bb4038d1b5f2f60557e00a3dc2c76bedb35
 
 const Home = () => {
     // const [currentSlide, setCurrentSlide] = useState(0);
@@ -65,7 +56,6 @@ const Home = () => {
 
     return (
         <section className="home container">
-<<<<<<< HEAD
             <Slider {...settings} className="slide">
                 {SLIDE2.map((slide) => {
                     return <img key={slide.id} src={slide.url} alt="" />;
@@ -80,24 +70,6 @@ const Home = () => {
                     <GrFormNext />
                 </div>
             </div> */}
-=======
-            <div className="slide">
-                <GrFormPrevious className="icon-left" />
-                <ul className="slider-dot">
-                    <li className="slider-dot-item active"></li>
-                    <li className="slider-dot-item"></li>
-                </ul>
-                <div className="myslide">
-                    <div className="slideItem">
-                        <img src={images1} alt="" />
-                    </div>
-                    <div className="slideItem">
-                        <img src={images2} alt="" />
-                    </div>
-                </div>
-                <GrFormNext className="icon-right" />
-            </div>
->>>>>>> ba136bb4038d1b5f2f60557e00a3dc2c76bedb35
             <div className="homeContent  ">
                 <div className="homeContentTitle">
                     <h3>ĐẶC QUYỀN CHO BẠN HÔM NAY!</h3>
